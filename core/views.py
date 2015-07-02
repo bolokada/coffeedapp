7437561f4dc4e2e1b6853c0170b9ff8e6854e78a
Kromah Fanta <fanta.kromah@etud.univ-montp2.fr>
2015-07-02 18:13:06 +0200

    ready to push

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+#create a new view
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+
+class LandingView (TemplateView):
+	template_name = "base/index.html"
