820267a85daaf48c1082711b94eb3c762818dd53
Kromah Fanta <fanta.kromah@etud.univ-montp2.fr>
2015-07-10 17:44:40 +0200

    some changes in settings for heroku admin

--- coffeedapp/settings.py
+++ coffeedapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django_sessions'
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
@@ -84,7 +85,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'NAME': os.path.join(BASE_DIR, '/tmp/db.sqlite3'),
     }
 }
 
